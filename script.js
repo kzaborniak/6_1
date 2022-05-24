0e8781cd78d5c5f7a8d0225dd121be3788c004ca
Klaudia Zaborniak <100691773+kzaborniak@users.noreply.github.com>
2022-05-24 11:42:55 +0000

    6_2 nieskończone

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 (function () {
   const ex1 = document.getElementById('ex1_button')
-  const ex1c = document.getElementById('ex1_content')
+  const ex1c = document.getElementById('ex1_content')                              
+  const ex2 = document.getElementById('ex2_txt')
+  const ex2c = document.getElementById('ex2_content') 
   ex1.addEventListener("click", function(){
     var data = "";
     for(var i = 0; i<10; i++){
@@ -11,4 +13,10 @@
     ex1c. innerHTML = data;
     
   })
-})();
+const poprawny = "Numer telefonu jest poprawny"
+  var nr = document.getElementById(ex2_txt);
+  const log = document.getElementById('ex2_content');
+  nr. addEventListener
+  
+  
+})();      
