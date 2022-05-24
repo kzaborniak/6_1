62b5811b7b5242ff331e28bf8aa257b602d3a458
Klaudia Zaborniak <100691773+kzaborniak@users.noreply.github.com>
2022-05-24 10:54:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+(function () {
+  const ex1 = document.getElementById('ex1_button')
+  const ex1c = document.getElementById('ex1_content')
+  ex1.addEventListener("click", function(){
+    var data = "";
+    for(var i = 0; i<10; i++){
+      if(i!=0)
+        data += ",";
+      data += i.toString();
+    }
+    ex1c. innerHTML = data;
+    
+  })
+})();
